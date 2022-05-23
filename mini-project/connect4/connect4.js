4c38c78c02fc88549eb5da5256e11beb11f71335
bellaclements <bella.clements@sainsburys.co.uk>
2022-05-23 15:44:16 +0100

    added connect 4 taketurn

--- mini-project/connect4/connect4.js
+++ mini-project/connect4/connect4.js
@@ -1,9 +1,166 @@
 let board = [
-    [null, null, null, null, null, null, null]
-    [null, null, null, null, null, null, null]
-    [null, null, null, null, null, null, null]
-    [null, null, null, null, null, null, null]
-    [null, null, null, null, null, null, null]
-    [null, null, null, null, null, null, null]
+    [null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null],
     [null, null, null, null, null, null, null]
 ]
+
+let redturn = true
+
+// Take the row and column number between 0 and 2
+// (inclusive) and update the game state.
+function takeTurn(row, column) {
+    console.log(`takeTurn was called with row: ${row}, column: ${column}`);
+    console.log(redturn);
+    console.log('row is ', row)
+    console.log('column is ', column)
+
+    if (board[row][column] === null && redturn === true){
+        board[row][column] = "red"
+        redturn = false
+    } else if (board[row][column]=== null) {
+        board[row][column] = "yellow"
+        redturn = true
+    }
+    else {
+        alert ('this space is taken')
+    }
+
+   
+}
+
+// Return either "noughts", "crosses" or "nobody" if the game is over.
+// Otherwise return null to continue playing.
+
+
+function checkWinner() {
+    const horizontalWinner = checkHorizontalWinner()
+    const verticalWinner = checkVerticalWinner ()
+    const diagonalWinner = checkDiagonalWinner ()
+    
+    if(horizontalWinner) {
+        return horizontalWinner
+    } else if (verticalWinner) {
+        return verticalWinner
+    } else if (diagonalWinner) {
+        return diagonalWinner
+    } else {
+        return null
+    }
+}
+ 
+    
+
+const checkHorizontalWinner = () => {
+    for(row =0; row <6;  row++){
+console.log('check row equals ', checkMatchingRow(row))
+        if (checkMatchingRow(row)) {
+            console.log('check row equals ', checkMatchingRow(row))
+            if(noughtsTurn === false) {
+                return "red"
+            } else {
+                return "yellow"
+            }
+        }
+    }
+}
+
+
+const checkMatchingRow = (row) => {
+    const firstColumn = board[row][0]
+    const secondColumn = board[row][1]
+    const thirdColumn = board[row][2]
+    
+    return firstColumn === secondColumn && firstColumn === thirdColumn && firstColumn !==null
+}
+
+const checkVerticalWinner = () => {
+    for(column =0; column <7; column ++){
+        console.log('check column equals ', checkMatchingColumn(column))
+
+        if (checkMatchingColumn(column)) {
+            console.log('check column equals ', checkMatchingColumn(column))
+           
+            if(redturn === false) {
+                return "red"
+            } else {
+                return "yellow"
+            }
+        }
+    }
+}
+
+const checkMatchingColumn = (column) => {
+    const firstRow = board [0][column]
+    const secondRow = board[1][column]
+    const thirdRow = board[2][column]
+    
+    return firstRow === secondRow && firstRow === thirdRow && firstRow !==null
+}
+
+
+
+const checkDiagonalWinner = () => {
+    
+    if (checkMatchingDiagonal()) {
+        if(redturn === false)  { 
+            return "red"
+        } else {
+            return "yellow" 
+        }
+    }
+}
+
+const checkMatchingDiagonal = () => {
+
+    const firstRow = board[0][0] // || board[2][0]
+    const secondRow = board[1][1]
+    const thirdRow = board[2][2] // || board [0][2]
+    const fourthRow = board[0][2]
+    const fifthRow = board[1][1]
+    const sixthRow = board[2][0]
+
+    return firstRow === secondRow && firstRow === thirdRow && firstRow !==null || fourthRow === secondRow && fifthRow === sixthRow && fourthRow !==null 
+   
+ }
+ 
+
+// Set the game state back to its original state to play another game.
+function resetGame(clearboard) {
+  
+    console.log("resetGame was called");
+    board = 
+    [null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null]
+}
+
+// Return the current board state with either a "nought" or a "cross" in
+// each position. Put a null in a position that hasn't been played yet.
+function getBoard() {
+    console.log("getBoard was called");
+    return board   
+ }
+
+
+if (typeof exports === 'object') {
+    console.log("Running in Node")
+    // Node. Does not work with strict CommonJS, but only CommonJS-like
+    // environments that support module.exports, like Node.
+    module.exports = {
+        takeTurn,
+        checkWinner,
+        resetGame,
+        getBoard,
+    }
+} else {
+    console.log("Running in Browser")
+}
+
--- mini-project/connect4/connect4_connectors.js
+++ mini-project/connect4/connect4_connectors.js
@@ -12,69 +12,75 @@ for (f of functions) { // f= index
 
 // Clear down the elements drawn on the board.
 function clearBoard() { //clear down elements 
-    for (let rowIndex = 0; rowIndex < 3; rowIndex++) { // for loop going thro each off the row index and column index and setting it to nothing
-        for (let columnIndex = 0; columnIndex < 3; columnIndex++) {
-            document.getElementById(`row-${rowIndex}-column-${columnIndex}`).innerHTML = "" // row index 
+    for (let rowIndex = 0; rowIndex < 5; rowIndex++) { // for loop going thro each off the row index and column index and setting it to nothing
+        for (let columnIndex = 0; columnIndex < 6; columnIndex++) { // inputs the column index, goes thro it all the column index and changes them to "" - not null
+            document.getElementById(`row-${rowIndex}-column-${columnIndex}`).innerHTML = "" // row index and column index innder HTMl changed to ""
         }
     }
 }
-
+// this function
 // Populate the grid with images based on the board state.
 function drawBoard(board) {
-    clearBoard();
+    clearBoard(); // changes everything to be ""
     //now recreate the board using the board valriable
-    for (let rowIndex = 0; rowIndex < 3; rowIndex++) {
-        for (let columnIndex = 0; columnIndex < 3; columnIndex++) {
-            if (!board[rowIndex][columnIndex]) {
+    for (let rowIndex = 0; rowIndex < 6; rowIndex++) { // goes thro row index and repopulated
+        for (let columnIndex = 0; columnIndex < 7; columnIndex++) { //goes thro coljmn index and repopulates
+            if (!board[rowIndex][columnIndex]) {  // if board doesnt have a thing in then u can place it 
                 continue;
             }
-            const cellText = board[rowIndex][columnIndex] === "nought" ? "⭕" : "❌";
-            document.getElementById(`row-${rowIndex}-column-${columnIndex}`).innerText = cellText;
+            const cellText = board[rowIndex][columnIndex] === "red" ? "🔴" : "🟡"; // variable cell text = point on board selected, they = red if it is red and if not it will show yello, ? is testing what si written, allows us to use true and false to put colours in. 
+            document.getElementById(`row-${rowIndex}-column-${columnIndex}`).innerText = cellText; // chnaging inner text to equal variable cell text based on column and row index, placing the counter on the boards
         }
     }
 }
 
+// helper functions, let you know when something has broken. 
 function isValidRowOrColumn(array) {
-    return Array.isArray(array) && array.length === 3;
+    return Array.isArray(array) && array.length === 6;
 }
 
 function isValidColumn(columnArray) {
-    return isValidRowOrColumn(columnArray) && columnArray.every(function (item) { return ["nought", "cross", null].includes(item); });
+    return isValidRowOrColumn(columnArray) && columnArray.every(function (item) { return ["red", "yellow", null].includes(item); });
 }
 
 // A grid position was clicked call the game's turn function, redraw and then check for a winner.
-function positionClick(rowIndex, columnIndex, event) {
-    takeTurn(rowIndex, columnIndex);
-    const board = getBoard();
-    if (!isValidRowOrColumn(board) || !board.every(isValidColumn)) {
-        throw "Expecting 'getBoard' to return a 2d array where all values match are null or one of the strings 'nought' or 'cross'. Actually received: " + JSON.stringify(board);
-    }
+function positionClick(rowIndex, columnIndex, event) { // check row column index based on event of clicking
+    takeTurn(rowIndex, columnIndex); //function using row index and column index
+    const board = getBoard(); //board always = get board
+    //if (!isValidRowOrColumn(board) || !board.every(isValidColumn)) { // chekcing board is a 2d array, links to 37-40 functions, if not showing yellow, red or null someting is an issue. 
+        //throw "Expecting 'getBoard' to return a 2d array where all values match are null or one of the strings 'yellow' or 'red'. Actually received: " + JSON.stringify(board);
+    
+
     drawBoard(board);
     const winner = checkWinner();
-    if (winner) {
-        if (typeof winner !== "string" || !["noughts", "crosses", "nobody"].includes(winner)) {
-            throw "Expecting 'checkWinner' to return null or one of the strings 'noughts', 'crosses' or 'nobody'. Actually received: " + winner;
+    if (winner) { 
+        // if not a string or the string  or not a string of red yellow or nobody then = false and throw error 
+        if (typeof winner !== "string" || !["red", "yellow", "nobody"].includes(winner)) {
+            throw "Expecting 'checkWinner' to return null or one of the strings 'yellow', 'red' or 'nobody'. Actually received: " + winner;
+         // bings back an error message
         }
-        const winnerName = document.getElementById("winner-name");
-        winnerName.innerText = winner;
-        const winnerDisplay = document.getElementById("winner-display");
-        winnerDisplay.style.display = "block";
+        const winnerName = document.getElementById("winner-name"); //changes the (document =HTML ) points to HTMl and gets the elemnt
+        winnerName.innerText = winner;// winner name - gets check winner function inner text (red/yellow = winner value)
+        const winnerDisplay = document.getElementById("winner-display"); // this is how it will showon html
+        winnerDisplay.style.display = "block"; //block of text is hidden till event lostener
     }
 }
 
-// The reset button was clicked, call the game's reset function then reset the DOM.
-function resetClick(event) {
+
+
+// The reset button was clicked, call the game's reset function then reset the DOM (document)
+function resetClick(event) { //when reset button is clicked
     resetGame();
-    const winnerName = document.getElementById("winner-name");
-    winnerName.innerText = "";
-    const winnerDisplay = document.getElementById("winner-display");
-    winnerDisplay.style.display = "None";
+    const winnerName = document.getElementById("winner-name"); // we are clearing winner name
+    winnerName.innerText = ""; // removing that from a screen
+    const winnerDisplay = document.getElementById("winner-display"); // clearing every winner function
+    winnerDisplay.style.display = "None"; //takes out the block 
     clearBoard();
 }
 
 // Bind the click events for the grid.
-for (let rowIndex = 0; rowIndex < 3; rowIndex++) {
-    for (let columnIndex = 0; columnIndex < 3; columnIndex++) {
+for (let rowIndex = 0; rowIndex < 6; rowIndex++) {
+    for (let columnIndex = 0; columnIndex < 7; columnIndex++) {
         const gridPosition = document.getElementById(`row-${rowIndex}-column-${columnIndex}`);
         gridPosition.addEventListener("click", positionClick.bind(null, rowIndex, columnIndex));
     }
